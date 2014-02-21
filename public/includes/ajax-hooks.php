5c2c6a5928a7205aa3117709ee6577dd31bc2113
Florian Grasset <florian.grasset@gmail.com>
2014-02-21 14:02:03 +0100

    Scope fixes & conflict correction.

--- public/includes/ajax-hooks.php
+++ public/includes/ajax-hooks.php
@@ -1,4 +1,5 @@
 <?php
+require_once( plugin_dir_path( __FILE__ ) . 'class-ajax-response.php' );
 /**
  * @package   LinkedIn_OAuth2
  * @author    Julien Grand <spoon4@gmail.com>
@@ -12,9 +13,9 @@ function get_linkedin_profile_ajax_action() {
 	if($token) {
 		$member = isset($_GET['member']) ? $_GET['member'] : LinkedInProfile::ME;
 		$profile = get_linkedin_profile($token, $member);
-		send_ajax_response(true, $profile);
+    LinkedInAjaxResponse::success($profile);
 	} else {
-		send_ajax_response(false, __('Invalid token'));
+    LinkedInAjaxResponse::error(__('Invalid token'));
 	}
 }
 
@@ -22,28 +23,13 @@ function post_linkedin_share_ajax_action() {
 	$token = get_linkedin_token();
 	if($token) {
 		$result = post_linkedin_share($token, $_POST);
-		send_ajax_response(true, $result);
+		LinkedInAjaxResponse::success($result);
 	} else {
-		send_ajax_response(false, __('Invalid token'));
+		LinkedInAjaxResponse::error(__('Invalid token'));
 	}
 }
 
 function linkedin_logout_ajax_action() {
 	clear_linkedin_data();
-	send_ajax_response(true, 'ok');
-}
-
-/**
- * Display a JSON encoded structure on standard input for AJAX responses.
- *
- * @param boolean $success The status of the response.
- * @param object $data The data of the response.
- *
- * @since    1.0.0
- */
-function send_ajax_response($success, $data)
-{
-	header('Content-Type: application/json');
-	echo json_encode(array('success' => $success, 'data' => $data));
-	exit;
+	LinkedInAjaxResponse::success('ok');
 }
